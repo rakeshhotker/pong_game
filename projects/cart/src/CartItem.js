3c56929a001018726bbc90b99a3df560a89da87a
raki <rakeshhotker@gmail.com>
2020-12-15 13:05:28 +0530

    Add constructor and object-destructuring

--- projects/cart/src/CartItem.js
+++ projects/cart/src/CartItem.js
@@ -1,18 +1,30 @@
 import React from 'react';
 class CartItem extends React.Component{
+    constructor()
+    {
+        super();
+        this.state={price:999,
+                    title:'Phone',
+                    qty:1,
+                    img:''}
+    }
     render()
     {
+        const {price,title,qty}=this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:'grey',fontSize:25}}>Rs 999</div>
-                    <div>Qty: 1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777',fontSize:25}}>Rs {price}</div>
+                    <div style={{color:'#777'}}>Qty:{qty}</div>
                     <div className="cart-items-actions">
-                        {/*Button*/}
+                        {/*Buttons*/}
+                        <img alt="increase" className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/992/992651.svg"/>
+                        <img alt="decrease" className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/992/992683.svg"/>
+                        <img alt="delete" className="action-icons" src="https://www.flaticon.com/svg/static/icons/svg/3096/3096673.svg"/>
                     </div>
                 </div>
             </div>
